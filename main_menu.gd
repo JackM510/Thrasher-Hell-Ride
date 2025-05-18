71bd6947e7fb0a54534cb9ca068d54aa2706f80a
JackM510 <nr@email.com>
2025-05-18 14:47:27 +1000

    Skeleton menu created

---
+++ main_menu.gd
@@ -0,0 +1,22 @@
+extends Control
+
+
+func story_mode_pressed() -> void:
+	pass # Replace with function body.
+	#get_tree().change_scene_to_file(filename-here)
+
+func survival_mode_pressed() -> void:
+	pass # Replace with function body.
+	print("Survival Mode pressed")
+
+func world_builder_pressed() -> void:
+	pass # Replace with function body.
+	print("Survival Mode pressed")
+
+func high_scores_pressed() -> void:
+	pass # Replace with function body.
+	print("Survival Mode pressed")
+
+func language_pressed() -> void:
+	pass # Replace with function body.
+	print("Survival Mode pressed")
